295a98dd078fdb9f128695bc5980c7a2440c01ed
Gustavo Mazzanti <gustavo.mazzanti@studio.unibo.it>
2022-09-07 16:56:38 +0200

    Solved error for import libraries

--- garden-service/src/http_manager/server.py
+++ garden-service/src/http_manager/server.py
@@ -1,9 +1,12 @@
 from flask import Flask, request
-from garden_manifest import Manifest
-from status import GardenStatus, IrrigatorStatus
 import sys
-
-sys.path.insert(0, './../manifest')
+import os
+import inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+from manifest.garden_manifest import Manifest
+from garden_status.status import *
 
 app = Flask(__name__)
 
@@ -18,7 +21,7 @@ def getData():
 
 @app.route('/dashboard/<path:path>')
 def get_dashboard_files(path):
-    return send_from_directory('garden-dashboard', path)
+    return send_from_directory('C:/Users/Gustavo/Documents/Uni/IOT/Assignement/assignement_03/garden-dashboard', path)
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000)
